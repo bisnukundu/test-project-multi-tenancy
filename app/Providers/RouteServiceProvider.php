a2dbd56b18318f49c12e9d4e47b49643a085bbfa
bisnukundu <bisnukundu@gmail.com>
2023-09-29 13:57:08 +0600

    I have done central domain 404 error solve

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -29,12 +29,6 @@ class RouteServiceProvider extends ServiceProvider
         });
 
         $this->routes(function () {
-            Route::middleware('api')
-                ->prefix('api')
-                ->group(base_path('routes/api.php'));
-
-            Route::middleware('web')
-                ->group(base_path('routes/web.php'));
             $this->mapApiRoutes();
             $this->mapWebRoutes();
         });
